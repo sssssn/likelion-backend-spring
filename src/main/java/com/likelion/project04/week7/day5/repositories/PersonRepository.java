a9c28fca0f50e74247d0485e2e99fdacdce0ab90
sssssn <doitforyourself0603@gmail.com>
2023-06-02 19:01:56 +0900

    feat: 강의 내용 실습 - jpa 데이터 액세스(REST)

---
+++ src/main/java/com/likelion/project04/week7/day5/repositories/PersonRepository.java
@@ -0,0 +1,17 @@
+package com.likelion.project04.week7.day5.repositories;
+
+import com.likelion.project04.week7.day5.models.Person;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends
+        PagingAndSortingRepository<Person, Long>,
+        CrudRepository<Person, Long> {
+
+    List<Person> findByLastName(@Param("name") String name);
+}
