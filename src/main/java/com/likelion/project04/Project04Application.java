2c1847bff5892a476335f89871e912768ca48b28
sssssn <doitforyourself0603@gmail.com>
2023-06-02 17:53:18 +0900

    feat: 강의 내용 실습 - Hello World!💛 index.html 추가 - GetMapping 로컬 호스트 주소/hello

--- src/main/java/com/likelion/project04/Project04Application.java
+++ src/main/java/com/likelion/project04/Project04Application.java
@@ -2,12 +2,21 @@ package com.likelion.project04;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Project04Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Project04Application.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "sssssn") String name) {
+		return "Hello " + name;
+	}
 }
