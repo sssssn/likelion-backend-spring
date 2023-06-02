9aa04003f18f91d1a453beffb6f93654b90b05d2
sssssn <doitforyourself0603@gmail.com>
2023-06-02 18:50:04 +0900

    feat: 강의 내용 실습 - Spring jpa sqlite 데이터 저장(findAll) - Customer, CustomerRepository 추가

--- src/main/java/com/likelion/project04/Project04Application.java
+++ src/main/java/com/likelion/project04/Project04Application.java
@@ -1,7 +1,13 @@
 package com.likelion.project04;
 
+import com.likelion.project04.week7.day5.models.Customer;
+import com.likelion.project04.week7.day5.repositories.CustomerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class Project04Application {
+
+	private static final Logger log = LoggerFactory.getLogger(Project04Application.class);
 
 	public static void main(String[] args) {
 
@@ -18,5 +26,17 @@ public class Project04Application {
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "sssssn") String name) {
 		return "Hello " + name;
+	}
+
+	@Bean
+	public CommandLineRunner demo(CustomerRepository repository) {
+		return args -> {
+			repository.save(new Customer("sn", "k"));
+
+			for (Customer customer : repository.findAll()) {
+				log.info(customer.toString());
+			}
+			log.info("");
+		};
 	}
 }
